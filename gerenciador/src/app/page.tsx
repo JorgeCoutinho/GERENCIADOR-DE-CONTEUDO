dda4c4c70a2dac6b6c95b2c84edd37cd4fc8297e
JORGE MIKAEL COUTINHO ALVES <jmca.lic19@uea.edu.br>
2024-08-25 14:22:23 -0300

    criando projeto

---
+++ gerenciador/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main >
+      <h1>TESTE PAGINA</h1>
+    </main>
+  );
+}
---
+++ gerenciador/src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+
+export function Header() {
+    return (
+        <header className={styles.container}>
+            <h1>Header</h1>
+        </header>
+    );
+}
+
