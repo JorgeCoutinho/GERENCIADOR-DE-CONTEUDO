95436c7ec7f82edb7eb501b0aec20c4e6e5b1713
JORGE MIKAEL COUTINHO ALVES <jmca.lic19@uea.edu.br>
2024-08-25 23:56:46 -0300

    criando o header fixed e background para ter efeito fixo

--- gerenciador/src/components/header/index.tsx
+++ gerenciador/src/components/header/index.tsx
@@ -1,10 +1,54 @@
-import styles from './styles.module.scss';
+"use client"
 
+import { useState, useEffect, use } from 'react';
+import styles from './styles.module.scss';
+import Link from 'next/link';
 
 export function Header() {
+    const [top, setTop] = useState(true);
+
+    const scrollHandler = () => {
+        window.scrollY > 100 ? setTop(false) : setTop(true)
+    }
+
+    useEffect(() => {
+
+        window.addEventListener('scroll', scrollHandler)
+
+        return () => window.removeEventListener('scroll', scrollHandler)
+
+    }, [top])
+
     return (
-        <header className={styles.container}>
-            <h1>Header</h1>
+        <header className={`${styles.header} ${!top ? styles.fixed : styles.background}`}>
+
+            <div className={styles.container}>
+
+                <div className={styles.content}>
+
+                    <div className={styles.contentLog}>
+                        <Link href='/'>
+                            MOTORS
+                        </Link>
+                    </div>
+
+                    <nav className={styles.nav}>
+                        <Link href='/'>
+                            HOME
+                        </Link>
+                        <Link href='/#servico'>
+                            SERVIÇOS
+                        </Link>
+                        <Link href='/#contatos'>
+                            CONTATOS
+                        </Link>
+
+                    </nav>
+
+                </div>
+
+            </div>
+
         </header>
     );
 }
